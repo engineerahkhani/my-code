f94d676b286ffe67d9cb97acd7222c06fcc3870a
engineeerahkhani <engineerahkhani@gmail.com>
2015-11-29 21:25:33 +0330

    6th commit its test too

--- app/src/main/java/com/example/ahmad/digiato/MovieDetailActivity.java
+++ app/src/main/java/com/example/ahmad/digiato/MovieDetailActivity.java
@@ -2,6 +2,7 @@ package com.example.ahmad.digiato;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.MenuItem;
 
 import com.example.ahmad.simplevolleytest.R;
 
@@ -12,6 +13,22 @@ public class MovieDetailActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail);
         getActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        if(savedInstanceState == null){
+
+            MovieDetailFragment fragment= new MovieDetailFragment();
+            getFragmentManager().beginTransaction()
+                    .add(R.id.frame_movie_detail, fragment)
+                    .commit();
+        }
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            finish();
+        }
+        return true;
+    }
 }
